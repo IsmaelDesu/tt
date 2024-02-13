ba1a4821f731da8000e9c680add84fd219390a62
IsmaelDesu <143649074+IsmaelDesu@users.noreply.github.com>
2024-02-13 09:36:11 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Style.css">
+    <title>EN TOUT CAS</title>
+</head>
+
+<body>
+    <script src="//pull.cappuccicons.com/cpf.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,500,700,900|Hind:300,400,500|Poppins:200,300,400,500,600,700,900|Quicksand|Playfair+Display:400,700,700i,900,900i|Montserrat:400,500,600,700,900|Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+
+    <div id="ths-wallpaper"></div>
+
+    <div class="ths-topics-list">
+
+        <div class="ths-topics-top">
+            <div class="ths-topics-head">
+                <div class="ths-topics-title">
+                    <h1>Pour toi Ma Sassy</h1>
+                </div>
+                <div class="ths-topics-btns" style="font-size: 14px;">
+                    Je n'ai pas pu faire un truc pousser pour manque de temps mais j'espère que tu vas appréciée !
+                </div>
+            </div>
+        </div>
+
+        <div class="ths-topics">
+
+            <div class="posts-header">
+                <h2 style="justify-content: center;">Présentation</h2>
+            </div>
+
+            <div class="ths-subjs">
+
+                <div class="ths-listed-topic colorpnj">
+
+                    <div class="listop-mid" style="height: 200px;">
+                        <div class="listop-gen" style="text-align: center;">
+                            <div class="listop-title" style="text-align: center; margin: auto;">
+                                <a href="/t778-event-de-l-arc-01-de-l-intrigue-explications">Pourquoi j'ai crée ce site
+                                    pour toi ?</a>
+                            </div>
+                        </div>
+                        <div class="listop-desc">
+                            <div style="font-size: 16px;">Parce que tu merite tout simplement haha. </br> </br>
+                                Plus serieusement, j'ai coder ce site juste pour te demander un petit truc a la fin donc
+                                t'as intéret a bien repondre hum hum !
+                                <Div style="font-size: 6px; margin-left: auto; text-align: center;">Et que je t'apprécie beaucoup</Div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="listop-right">
+                        <div class="sub-ava" style="width: 150px; height : 150px; text-align: center; margin: auto;">
+                            <div id="last-ava" style="width: 130px; height : 130px">
+                                <img loading="lazy" src="/Fleur1.jpg" style="max-width: 200px; max-height: 400px">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="posts-header">
+                <h2>Suivant</h2>
+            </div>
+
+            <div class="ths-subjs">
+
+                <div class="ths-listed-topic">
+
+                    <div class="listop-mid" style="height: 370px;">
+                        <div class="listop-gen">
+                            <div class="listop-title" style="text-align: center; margin: auto;">
+                                <a href="/t778-event-de-l-arc-01-de-l-intrigue-explications" style="font-size: 20px; margin:0 auto;">Petite poeme</a>
+                            </div>
+                        </div>
+                        <div class="listop-desc">
+                            <div>
+                                <div>
+                                    <h3 style="text-align: justify;">Quand je t'ai vu pour la premi&egrave;re fois,
+                                        Sassy,</h3> </br>
+                                    <h3 style="text-align: justify;">J'ai vu ta t&ecirc;te bien aigrie, bien refroidie.
+                                    </h3></br>
+                                    <h3 style="text-align: justify;">N'emp&ecirc;che, quand on a mang&eacute; ensemble
+                                        et fait nos balades sur paris,</h3></br>
+                                    <h3 style="text-align: justify;">La journ&eacute;e est pass&eacute;e vite, tellement
+                                        j'ai appr&eacute;ci&eacute; ta compagnie,</h3></br>
+                                    <h3 style="text-align: justify;">Donc, si je fais ce po&egrave;me de tonton,</h3>
+                                    </br>
+                                    <h3 style="text-align: justify;">C'est juste pour que tu puisses dire oui, (&agrave;
+                                        la fin de ce site)</h3></br>
+                                    <h3 style="text-align: justify;">Et que nos chemins se suivent, s'embellissent,
+                                        s'unifient. (Un peu trop j'avoue)</h3></br>
+                                </div>
+                                <div style="margin: auto; text-align: center;">Voila voila, esperes pas plus non plus ok</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="listop-right">
+                        <div class="sub-ava" style="width: 220px; height : 220px; text-align: center; margin: auto;">
+                            <div id="last-ava" style="width: auto;  height : auto">
+                                <img loading=" lazy" src="/cat.png" style="text-align: center; margin-top: 8px;">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="posts-header">
+                <h2>La fameuse question</h2>
+            </div>
+
+            <div class="ths-subjs">
+
+                <div class="ths-listed-topic">
+
+                    <div class="listop-mid">
+                        <div class="listop-gen">
+                            <div class="listop-title" style="text-align: center; margin: auto;">
+                                <a href="/t778-event-de-l-arc-01-de-l-intrigue-explications">Voudrais-tu être ma
+                                    valentine Lindsey ?</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="listop-right">
+                        <div class="sub-ava" style="text-align: center; margin: auto;">
+                            <div id="last-ava">
+                                <img loading="lazy" src="/Fleur2.jpg" style="max-width: 200px; max-height: 400px">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="listop-mid" style="margin : auto; display : flex; height: 300px; gap: -20px;">
+                <Div style="margin: auto; text-align :center; ">
+                    <img src='/cat.gif' alt='cat asking question' class="responsive" style="width: 220px;">
+                </Div>
+                <div class="response-buttons" style="margin: auto; text-align :center; display : flex; gap: 5px;">
+                    <form action="/lastpage.php" method="post">
+                        <input type="hidden" name="response" value="yes">
+                        <button id="yesButton" type="submit">Oui</button>
+                    </form>
+                    <button id="noButton">Non</button>
+                </div>
+            </div>
+
+            </br>
+
+            <div>
+            </div>
+
+            </br>
+
+
+            <div>
+            </div>
+            <script>
+                document.getElementById('noButton').addEventListener('click', function() {
+                    var yesButton = document.getElementById('yesButton');
+                    var currentFontSize = parseInt(window.getComputedStyle(yesButton).fontSize);
+                    yesButton.style.fontSize = (currentFontSize + 10) + 'px'; // Increase font size by 5px
+                });
+            </script>
+</body>
+
+</html>
